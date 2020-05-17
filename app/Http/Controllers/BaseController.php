6571ed0117dea6ca3fcc2c8e8a55f000067f0a80
Pharoouzy <pharoouzy@yahoo.com>
2020-05-17 20:35:20 +0100

    Add BaseController handling redirects and responses

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -40,7 +40,45 @@ class BaseController extends Controller {
      * @param null $data
      * @return \Illuminate\Http\JsonResponse
      */
-    protected function responseJson($error = true, $responseCode = 200){
+    protected function responseJson($error = true, $responseCode = 200, $message = [], $data = null){
+        return response()->json([
+            'error' => $error,
+            'response_code' => $responseCode,
+            'message' => $message,
+            'data' => $data
+        ], $responseCode);
+    }
 
+    /**
+     * @param $route
+     * @param $message
+     * @param string $type
+     * @param bool $error
+     * @param bool $withOldInputWhenError
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function responseRedirect($route, $message, $type = 'info', $error = false, $withOldInputWhenError = false){
+        $this->setFlashMessage($message, $type);
+        $this->showFlashMessages();
+
+        if($error && $withOldInputWhenError){
+            return redirect()->back()->withInput();
+        }
+
+        return redirect()->route($route);
+    }
+
+    /**
+     * @param $message
+     * @param string $type
+     * @param bool $error
+     * @param bool $withOldInputWhenError
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function responseRedirectBack($message, $type = 'info', $error = false, $withOldInputWhenError = false) {
+        $this->setFlashMessage($message, $type);
+        $this->showFlashMessages();
+
+        return redirect()->back();
     }
 }
