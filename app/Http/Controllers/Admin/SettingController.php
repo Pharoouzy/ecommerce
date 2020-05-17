6deb38ef9e7d8a300f67567a1a502f324a763b3d
Pharoouzy <pharoouzy@yahoo.com>
2020-05-17 23:23:22 +0100

    Create UploadAble trait for handling File uploading

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Traits\UploadAble;
 use App\Http\Controllers\BaseController;
 
 class SettingController extends BaseController {
+
+    use UploadAble;
 
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
