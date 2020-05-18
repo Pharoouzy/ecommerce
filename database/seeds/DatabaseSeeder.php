0084084e62b115ed8613cc259d82df6273aefc5d
Pharoouzy <pharoouzy@yahoo.com>
2020-05-18 17:04:18 +0100

    Create Seeder and Factory-Faker for Category

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SettingsTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
     }
 }
