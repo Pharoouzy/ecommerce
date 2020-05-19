21e7304156df9960927441e6f1faac89e43eab60
Pharoouzy <pharoouzy@yahoo.com>
2020-05-20 00:02:04 +0100

    Complete CategoryRespository class

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -19,7 +19,111 @@
     class CategoryRepository extends BaseRepository implements CategoryContract {
         use UploadAble;
 
-        public function __construct(Model $model) {
+        /**
+         * CategoryRepository constructor.
+         * @param Category $model
+         */
+        public function __construct(Category $model) {
+            parent::__construct($model);
+            $this->model = $model;
+        }
 
+        /**
+         * @param string $order
+         * @param string $sort
+         * @param array $columns
+         * @return mixed
+         */
+        public function listCategories(string $order = 'id', string $sort = 'desc', array $columns = ['*']) {
+            return $this->all($columns, $order, $sort);
+        }
+
+        /**
+         * @param int $id
+         * @return mixed
+         * @throws ModelNotFoundException
+         */
+        public function findCategoryById(int $id) {
+            try {
+                return $this->findOneOrFail($id);
+
+            } catch (ModelNotFoundException $e) {
+
+                throw new ModelNotFoundException($e);
+            }
+        }
+
+        /**
+         * @param array $data
+         * @return Category|mixed
+         */
+        public function createCategory(array $data) {
+            try {
+                $collection = collect($data);
+
+                $image = null;
+
+                if ($collection->has('image') && ($data['image'] instanceof  UploadedFile)) {
+                    $image = $this->uploadOne($data['image'], 'categories');
+                }
+
+                $featured = $collection->has('featured') ? 1 : 0;
+                $menu = $collection->has('menu') ? 1 : 0;
+
+                $merge = $collection->merge(compact('menu', 'image', 'featured'));
+
+                $category = new Category($merge->all());
+
+                $category->save();
+
+                return $category;
+
+            } catch (QueryException $exception) {
+                throw new InvalidArgumentException($exception->getMessage());
+            }
+        }
+
+        /**
+         * @param array $data
+         * @return mixed
+         */
+        public function updateCategory(array $data) {
+            $category = $this->findCategoryById($data['id']);
+
+            $collection = collect($data)->except('_token');
+
+            if ($collection->has('image') && ($data['image'] instanceof  UploadedFile)) {
+
+                if ($category->image != null) {
+                    $this->deleteOne($category->image);
+                }
+
+                $image = $this->uploadOne($data['image'], 'categories');
+            }
+
+            $featured = $collection->has('featured') ? 1 : 0;
+            $menu = $collection->has('menu') ? 1 : 0;
+
+            $merge = $collection->merge(compact('menu', 'image', 'featured'));
+
+            $category->update($merge->all());
+
+            return $category;
+        }
+
+        /**
+         * @param $id
+         * @return bool|mixed
+         */
+        public function deleteCategory($id) {
+            $category = $this->findCategoryById($id);
+
+            if ($category->image != null) {
+                $this->deleteOne($category->image);
+            }
+
+            $category->delete();
+
+            return $category;
         }
     }
