d09f583b86e690d791f88909a602aae8e50eaf90
Pharoouzy <pharoouzy@yahoo.com>
2020-06-07 17:26:40 +0100

    Update AttributesTable seeder

--- database/seeds/AttributesTableSeeder.php
+++ database/seeds/AttributesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Attribute;
 use Illuminate\Database\Seeder;
 
 class AttributesTableSeeder extends Seeder
@@ -9,8 +10,23 @@ class AttributesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        // Create a size attribute
+        Attribute::create([
+            'code'          =>  'size',
+            'name'          =>  'Size',
+            'frontend_type' =>  'select',
+            'is_filterable' =>  1,
+            'is_required'   =>  1,
+        ]);
+
+        // Create a color attribute
+        Attribute::create([
+            'code'          =>  'color',
+            'name'          =>  'Color',
+            'frontend_type' =>  'select',
+            'is_filterable' =>  1,
+            'is_required'   =>  1,
+        ]);
     }
 }
