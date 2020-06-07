380e0378e98639bbd5e4f70031a6832227717cbd
Pharoouzy <pharoouzy@yahoo.com>
2020-06-07 17:24:30 +0100

    Create AttributesTable seeder for seeding attribute data

---
+++ database/seeds/AttributesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AttributesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
