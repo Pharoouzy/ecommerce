9a0193d1bfc784a02640bd558d3ee7de12101598
Pharoouzy <pharoouzy@yahoo.com>
2020-06-13 22:59:13 +0100

    Modify Model and migration for Attribute Values

--- app/Models/AttributeValue.php
+++ app/Models/AttributeValue.php
@@ -4,7 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class AttributeValue
+ * @package App\Models
+ */
 class AttributeValue extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $table = 'attribute_values';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'attribute_id', 'value', 'price'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'attribute_id'  =>  'integer',
+    ];
 }
