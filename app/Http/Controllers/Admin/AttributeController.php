d723fc9dc800c6ac65bad0b047eeefc0b66b0c1f
Pharoouzy <pharoouzy@yahoo.com>
2020-06-14 00:57:52 +0100

    Add index method for displaying all attributes

--- app/Http/Controllers/Admin/AttributeController.php
+++ app/Http/Controllers/Admin/AttributeController.php
@@ -12,9 +12,26 @@ use App\Http\Controllers\BaseController;
  */
 class AttributeController extends BaseController
 {
+    /**
+     * @var AttributeContract
+     */
     protected $attributeRepository;
 
+    /**
+     * AttributeController constructor.
+     * @param AttributeContract $attributeRepository
+     */
     public function __construct(AttributeContract $attributeRepository) {
         $this->attributeRepository = $attributeRepository;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index() {
+        $attributes = $this->attributeRepository->listAttributes();
+
+        $this->setPageTitle('Attributes', 'List of all attributes');
+        return view('admin.attributes.index', compact('attributes'));
     }
 }
